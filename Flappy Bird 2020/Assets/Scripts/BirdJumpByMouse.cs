98b7a73ad3f8c0d73d7d3d912478b2262673a3cf
daonq2001 <daonq.nde19046@vtc.edu.vn>
2020-09-13 08:58:16 +0700

    Bird Jump By Mouse

---
+++ Flappy Bird 2020/Assets/Scripts/BirdJumpByMouse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BirdJumpByMouse : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    public float jumpForce = 10f;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if(rb2D.velocity.y != jumpForce)
+            {
+                rb2D.velocity = new Vector2(rb2D.transform.position.x, 10f);
+            }
+        }
+    }
+}
